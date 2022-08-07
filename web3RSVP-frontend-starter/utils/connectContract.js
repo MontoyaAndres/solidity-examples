34c6c07e51a62b17311839d3a404bccec102fec2
MontoyaAndres <andresmontoyafcb@gmail.com>
2022-08-07 09:10:37 -0500

    Feat: create event functionality

--- web3RSVP-frontend-starter/utils/connectContract.js
+++ web3RSVP-frontend-starter/utils/connectContract.js
@@ -4,23 +4,24 @@ import abiJSON from "./Web3RSVP.json";
 
 function connectContract() {
   const contractAddress = "0xc7D259Bb8Ee9281857897164AD7D444b007b7E5F";
-  const contractABI = abiJSON.abi;
+  const contractABI = abiJSON;
   let rsvpContract;
 
   try {
     const { ethereum } = window;
 
-    if (ethereum) {
+    if (ethereum.chainId === "0x13881") {
+      //checking for eth object in the window, see if they have wallet connected to Mumbai network
       const provider = new ethers.providers.Web3Provider(ethereum);
       const signer = provider.getSigner();
-      rsvpContract = new ethers.Contract(contractAddress, contractABI, signer);
+      console.log("contractABI", contractABI);
+      rsvpContract = new ethers.Contract(contractAddress, contractABI, signer); // instantiating new connection to the contract
     } else {
-      console.log("Ethereum object doesn't exist!");
+      throw new Error("Please connect to the Polygon Mumbai network.");
     }
   } catch (error) {
     console.log("ERROR:", error);
   }
-
   return rsvpContract;
 }
 
