2abfedc67ffaed4f63638954e30065a52956b582
MontoyaAndres <andresmontoyafcb@gmail.com>
2023-04-23 15:04:37 -0500

    Feat: arithmetic-overflows-3

---
+++ sch-solutions-part-1/contracts/arithmetic-overflows-3/AIvestICO.sol
@@ -0,0 +1,65 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.7.0;
+
+import "./AIvestToken.sol";
+
+contract AIvestICO {
+    uint256 public constant SALE_PERIOD = 3 days;
+
+    AIvestToken public token;
+    uint256 public startTime;
+    address public admin;
+
+    modifier onlyAdmin() {
+        require(msg.sender == admin, "not admin");
+        _;
+    }
+
+    constructor() {
+        token = new AIvestToken();
+        admin = msg.sender;
+        startTime = block.timestamp;
+    }
+
+    function buy(uint256 numTokens) public payable {
+        require(block.timestamp <= startTime + SALE_PERIOD, "ICO is over");
+
+        // 1 ETH = 10 Tokens (1 Token = 0.1 ETH)
+        require(msg.value == (numTokens * 10) / 100, "wrong ETH amount sent");
+
+        token.mint(msg.sender, numTokens);
+    }
+
+    function refund(uint256 numTokens) public {
+        require(block.timestamp < startTime + SALE_PERIOD, "ICO is over");
+
+        token.burn(msg.sender, numTokens);
+
+        // 1 ETH = 10 Tokens (1 Token = 0.1 ETH)
+        (bool success, ) = payable(msg.sender).call{
+            value: (numTokens * 10) / 100
+        }("");
+
+        require(success);
+    }
+
+    function adminWithdraw() external onlyAdmin {
+        require(
+            block.timestamp > startTime + SALE_PERIOD,
+            "only when sale is over"
+        );
+        (bool success, ) = payable(msg.sender).call{
+            value: address(this).balance
+        }("");
+        require(success);
+    }
+
+    function adminMint(address _to, uint256 _amount) external onlyAdmin {
+        token.mint(_to, _amount);
+    }
+
+    function changeAdmin(address _newAdmin) external onlyAdmin {
+        require(_newAdmin != address(0));
+        admin = _newAdmin;
+    }
+}
