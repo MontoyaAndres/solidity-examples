3adbc38f61c02a410ade7a761ef37ce7c19a0791
MontoyaAndres <andresmontoyafcb@gmail.com>
2023-04-22 20:37:53 -0500

    Feat: arithmetic-overflows-2

---
+++ sch-solutions-part-1/contracts/arithmetic-overflows-2/solution/SimpleTokenSecured.sol
@@ -0,0 +1,24 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.13;
+
+contract SimpleToken {
+    address public minter;
+    mapping(address => uint) public getBalance;
+    uint public totalSupply;
+
+    constructor() {
+        minter = msg.sender;
+    }
+
+    function mint(address _to, uint _amount) external {
+        require(msg.sender == minter, "not minter");
+        getBalance[_to] += _amount;
+    }
+
+    function transfer(address _to, uint _value) public returns (bool) {
+        require(getBalance[msg.sender] - _value >= 0);
+        getBalance[msg.sender] -= _value;
+        getBalance[_to] += _value;
+        return true;
+    }
+}
